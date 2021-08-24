106daa1dda056af23acc1b8210b0aab1aab9dad8
Hieu Khuong <ktgiahieu@gmail.com>
2021-08-24 16:42:38 +0700

    Update utils.py

--- src/1st_level/xlmr_base_baseline/utils.py
+++ src/1st_level/xlmr_base_baseline/utils.py
@@ -22,7 +22,7 @@ def postprocess_qa_predictions(examples, features, raw_predictions, n_best_size 
     example_id_to_index = {k: i for i, k in enumerate(examples["id"])}
     features_per_example = collections.defaultdict(list)
     for i, feature in enumerate(features):
-        features_per_example[example_id_to_index[feature["example_id"]]].append(i)
+        features_per_example[example_id_to_index[feature["example_ids"]]].append(i)
 
     predictions = collections.OrderedDict()
 
