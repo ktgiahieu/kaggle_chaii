511cdabd89bab8fc999965f454066801da68b2b6
Hieu Khuong <ktgiahieu@gmail.com>
2021-08-25 13:35:13 +0700

    add

--- src/1st_level/xlmr_base_squad2_classifier/config.py
+++ src/1st_level/xlmr_base_squad2_classifier/config.py
@@ -65,7 +65,7 @@ SWA_FREQ = 30
 
 SAVE_CHECKPOINT_TYPE = 'best_iter' #'best_iter', 'best_epoch' or 'last_epoch'
 EVAL_SCHEDULE = [
-                (300., 1*ACCUMULATION_STEPS),
+                (10., 300*ACCUMULATION_STEPS),
                 ]
 
 
