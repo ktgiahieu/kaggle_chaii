303613003f55c689785d3f88a9d918e163549bb9
Hieu Khuong <ktgiahieu@gmail.com>
2021-09-01 10:15:28 +0700

    Update dataset.py

--- src/1st_level/xlmr_large_squad2_baseline_320/dataset.py
+++ src/1st_level/xlmr_large_squad2_baseline_320/dataset.py
@@ -84,14 +84,20 @@ def preprocess_data(tokenizer, ids, contexts, questions, answers, answer_starts)
                 for i in range(token_answer_start_index, targets_end + 1):
                     jac = jaccard_array(answer_array, sentence_array[i:targets_end + 1])
                     start_labels[i] = jac + jac ** 2
-                start_labels = (1 - config.SOFT_ALPHA) * start_labels / start_labels.sum()
+                if start_labels.sum() == 0:
+                    start_labels = np.zeros(n)
+                else:
+                    start_labels = (1 - config.SOFT_ALPHA) * start_labels / start_labels.sum()
                 start_labels[targets_start] += config.SOFT_ALPHA
 
                 end_labels = np.zeros(n)
                 for i in range(targets_start, token_answer_end_index + 1):
                     jac = jaccard_array(answer_array, sentence_array[targets_start:i + 1])
                     end_labels[i] = jac + jac ** 2
-                end_labels = (1 - config.SOFT_ALPHA) * end_labels / end_labels.sum()
+                if end_labels.sum() == 0:
+                    end_labels = np.zeros(n)
+                else:
+                    end_labels = (1 - config.SOFT_ALPHA) * end_labels / end_labels.sum()
                 end_labels[targets_end] += config.SOFT_ALPHA
 
                 start_labels = list(start_labels)
