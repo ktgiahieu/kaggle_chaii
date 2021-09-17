32d417157a35fe92f13599a2f4d90d1fb60db54d
Hieu Khuong <ktgiahieu@gmail.com>
2021-09-17 21:32:44 +0700

    Update config.py

--- src/1st_level/muril_c1_u_pretrained_ce/config.py
+++ src/1st_level/muril_c1_u_pretrained_ce/config.py
@@ -73,5 +73,5 @@ EVAL_SCHEDULE = [
 #Layer wise learning rate
 HEAD_LEARNING_RATE = 5e-4
 LEARNING_RATE_LAYERWISE_TYPE = 'exponential' #'linear' or 'exponential'
-LEARNING_RATES_RANGE = [1e-5, 5e-5]
+LEARNING_RATES_RANGE = [5e-5, 1e-4]
 WEIGHT_DECAY = 0.01
