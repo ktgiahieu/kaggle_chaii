81781dee4db4ef5b3d2136a8b8f52e2eeac726e3
Hieu Khuong <ktgiahieu@gmail.com>
2021-09-17 16:14:36 +0700

    Update evaluate_prob.py

--- src/pretrained/muril_combo1_external_ce/evaluate_prob.py
+++ src/pretrained/muril_combo1_external_ce/evaluate_prob.py
@@ -29,7 +29,7 @@ def run(fold):
 
     seed_models = []
     for seed in config.SEEDS:
-        model = models.ChaiiClassifierModel(conf=model_config)
+        model = models.ChaiiModel(conf=model_config)
         model.to(device)
         model.load_state_dict(torch.load(
             f'{config.TRAINED_MODEL_PATH}/model.bin'),
