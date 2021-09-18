1f3f1fd41833c617e6a27720edca25349608e868
Hieu Khuong <ktgiahieu@gmail.com>
2021-09-18 17:24:50 +0700

    add

---
+++ src/pretrained/xlmr_base_squad2_c1_external_ce/config.py
@@ -0,0 +1,76 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning) 
+
+import tokenizers
+from transformers import AutoTokenizer, AutoConfig
+import os
+is_kaggle = 'KAGGLE_URL_BASE' in os.environ
+
+# Paths
+comp_name = 'chaii-hindi-and-tamil-question-answering'
+my_impl = 'chaii-impl'
+my_model_dataset = 'chaii-xlmr-base-squad2-c1-external-ce'
+if is_kaggle:
+    TRAINING_FILE = f'../input/{my_impl}/data/external.csv'
+    VALID_FILE = f'../input/{my_impl}/data/train_folds_cleaned.csv'
+    TEST_FILE = f'../input/{comp_name}/test.csv'
+    SUB_FILE = f'../input/{comp_name}/sample_submission.csv'
+    MODEL_SAVE_PATH = f'.'
+    TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
+    INFERED_PICKLE_PATH = '.'
+
+    MODEL_CONFIG = '../input/deepset-xlm-roberta-base-squad2'
+else: #colab
+    repo_name = 'kaggle_chaii'
+    drive_name = 'Chaii'
+    model_save = 'xlmr_base_squad2_c1_external_ce'
+    
+    TRAINING_FILE = f'/content/{repo_name}/data/external.csv'
+    VALID_FILE = f'/content/{repo_name}/data/train_folds_cleaned.csv'
+    TEST_FILE = f'/content/{repo_name}/data/test.csv'
+    SUB_FILE = f'/content/{repo_name}/data/sample_submission.csv'
+    MODEL_SAVE_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/{model_save}'
+    TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/{model_save}'
+    INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
+
+    MODEL_CONFIG = 'deepset/xlm-roberta-base-squad2'
+
+# Model params
+SEEDS = [1000]
+EPOCHS = 5
+
+PATIENCE = None
+EARLY_STOPPING_DELTA = None
+TRAIN_BATCH_SIZE = 16
+VALID_BATCH_SIZE = 16
+ACCUMULATION_STEPS = 1
+MAX_LEN = 384
+DOC_STRIDE = 128
+
+TOKENIZER = AutoTokenizer.from_pretrained(
+    MODEL_CONFIG)
+
+CONF = AutoConfig.from_pretrained(
+    MODEL_CONFIG)
+
+N_LAST_HIDDEN = 4
+BERT_DROPOUT = 0.1
+HIGH_DROPOUT = 0.5
+SOFT_ALPHA = 1.0
+WARMUP_RATIO = 0.1
+
+USE_SWA = False
+SWA_RATIO = 0.9
+SWA_FREQ = 30
+
+SAVE_CHECKPOINT_TYPE = 'best_iter' #'best_iter', 'best_epoch' or 'last_epoch'
+EVAL_SCHEDULE = [
+                (10., 200*ACCUMULATION_STEPS),
+                ]
+
+
+#Layer wise learning rate
+HEAD_LEARNING_RATE = 5e-5
+LEARNING_RATE_LAYERWISE_TYPE = 'exponential' #'linear' or 'exponential'
+LEARNING_RATES_RANGE = [2e-5, 5e-5]
+WEIGHT_DECAY = 0.01
