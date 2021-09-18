41aa04023e6d3f9d8e2b1e49ec1b34d320fc1b1e
Hieu Khuong <ktgiahieu@gmail.com>
2021-09-18 10:16:26 +0700

    Update evaluate_prob.py

--- src/1st_level/muril_c1_u_pretrained_ce/evaluate_prob.py
+++ src/1st_level/muril_c1_u_pretrained_ce/evaluate_prob.py
@@ -81,7 +81,7 @@ def run(fold):
             outputs_seeds = []
             for i in range(len(config.SEEDS)):
                 outputs_start, outputs_end = seed_models[i](ids=ids, mask=mask)
-                outputs = torch.clamp(1 - (torch.softmax(outputs_start[:,:-1],dim=1)[:,0] + torch.softmax(outputs_end[:,:-1],dim=1)[:,0])/2, min=0.5, max=1)
+                outputs = (torch.clamp(1 - (torch.softmax(outputs_start[:,:-1],dim=1)[:,0] + torch.softmax(outputs_end[:,:-1],dim=1)[:,0])/2, min=0.5, max=1) -0.5)*2
                 outputs_seeds.append(outputs)
 
             outputs = sum(outputs_seeds) / (len(config.SEEDS))
