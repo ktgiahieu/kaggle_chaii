1d489cac3ca50b81b959836fad3cde68853cf6a2
Hieu Khuong <ktgiahieu@gmail.com>
2021-09-23 17:09:52 +0700

    Update config.py

--- src/1st_level/muril_c1_toext_diff_hns/config.py
+++ src/1st_level/muril_c1_toext_diff_hns/config.py
@@ -18,7 +18,7 @@ if is_kaggle:
     TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
     INFERED_PICKLE_PATH = '.'
 
-    MODEL_CONFIG = '../input/deepset-xlm-roberta-base-squad2'
+    MODEL_CONFIG = '../input/google-muril-base-cased-squad2'
 else: #colab
     repo_name = 'kaggle_chaii'
     drive_name = 'Chaii'
@@ -33,7 +33,7 @@ else: #colab
     TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
 
-    MODEL_CONFIG = 'deepset/xlm-roberta-base-squad2'
+    MODEL_CONFIG = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/google-muril-base-cased-squad2'
 
 # Model params
 SEEDS = [13, 26, 43, 1001, 2022]
