e71cded3f43ad1b56d02282db93fc142f933c9eb
Hieu Khuong <ktgiahieu@gmail.com>
2021-10-15 09:45:04 +0700

    add

--- src/1st_level/3f_bert_c1_ext_wu/config.py
+++ src/1st_level/3f_bert_c1_ext_wu/config.py
@@ -9,34 +9,35 @@ is_kaggle = 'KAGGLE_URL_BASE' in os.environ
 # Paths
 comp_name = 'chaii-hindi-and-tamil-question-answering'
 my_impl = 'chaii-impl'
-my_model_dataset = 'chaii-3f-xlmrbs2-c1-toext-wu'
+my_model_dataset = 'chaii-3f-bert-c1-ext-wu'
 if is_kaggle:
-    TRAINING_FILE = f'../input/{my_impl}/data/3f_train_folds_external_cleaned_dropped.csv'
+    TRAINING_FILE = f'../input/{my_impl}/data/external_dropped.csv'
     TEST_FILE = f'../input/{comp_name}/test.csv'
     SUB_FILE = f'../input/{comp_name}/sample_submission.csv'
     MODEL_SAVE_PATH = f'.'
     TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
     INFERED_PICKLE_PATH = '.'
 
-    MODEL_CONFIG = '../input/deepset-xlm-roberta-base-squad2'
+    MODEL_CONFIG = '../input/bert-base-multilingual-cased-squad2'
 else: #colab
     repo_name = 'kaggle_chaii'
     drive_name = 'Chaii'
-    model_save = '3f_xlmrbs2_c1_toext_wu'
+    model_save = '3f_bert_c1_ext_wu'
     
-    TRAINING_FILE = f'/content/{repo_name}/data/3f_train_folds_external_cleaned_dropped.csv'
+    TRAINING_FILE = f'/content/{repo_name}/data/external_dropped.csv'
+    VALID_FILE = f'/content/{repo_name}/data/3f_train_folds_external_cleaned_dropped.csv'
     TEST_FILE = f'/content/{repo_name}/data/test.csv'
     SUB_FILE = f'/content/{repo_name}/data/sample_submission.csv'
     MODEL_SAVE_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
 
-    MODEL_CONFIG = 'deepset/xlm-roberta-base-squad2'
+    MODEL_CONFIG = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/bert-base-multilingual-cased-squad2'
 
 # Model params
-SEEDS = [44, 1002, 2023]
+SEEDS = [48, 1106, 2027]
 N_FOLDS = 3
-EPOCHS = 4
+EPOCHS = 10
 #NEGATIVE_POSITIVE_RATIO = 3.0
 
 PATIENCE = None
@@ -54,24 +55,24 @@ TOKENIZER = AutoTokenizer.from_pretrained(
 CONF = AutoConfig.from_pretrained(
     MODEL_CONFIG)
 
-N_LAST_HIDDEN = [12,  8,  4]
+N_LAST_HIDDEN = [12,  6,  9]
 BERT_DROPOUT = 0.1
-HIGH_DROPOUT = [0.3 , 0.45, 0.4 ]
-SOFT_ALPHA = [1. , 0.6, 0.9]
+HIGH_DROPOUT = [0.3, 0.4, 0.45]
+SOFT_ALPHA = [0.8, 0.9, 0.6]
 WARMUP_RATIO = 0.1
 
 USE_SWA = False
 SWA_RATIO = 0.9
 SWA_FREQ = 30
 
-SAVE_CHECKPOINT_TYPE = 'best_epoch' #'best_iter', 'best_epoch' or 'last_epoch'
+SAVE_CHECKPOINT_TYPE = 'last_epoch' #'best_iter', 'best_epoch' or 'last_epoch'
 EVAL_SCHEDULE = [
                 (10., 200*ACCUMULATION_STEPS),
                 ]
 
 
 #Layer wise learning rate
-HEAD_LEARNING_RATE = 5e-5
+HEAD_LEARNING_RATE = 3e-5
 LEARNING_RATE_LAYERWISE_TYPE = 'exponential' #'linear' or 'exponential'
-LEARNING_RATES_RANGE = [1e-5, 4e-5]
+LEARNING_RATES_RANGE = [3e-5, 3e-5]
 WEIGHT_DECAY = 0.001
