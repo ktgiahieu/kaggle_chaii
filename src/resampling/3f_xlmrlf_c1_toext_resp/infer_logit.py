c58e3bf4ec130dfa9741feab76b9b6648daac0ec
Hieu Khuong <ktgiahieu@gmail.com>
2021-10-24 18:10:40 +0700

    add

--- src/resampling/3f_xlmrlf_c1_toext_resp/infer_logit.py
+++ src/resampling/3f_xlmrlf_c1_toext_resp/infer_logit.py
@@ -19,7 +19,7 @@ def run():
     df_test = pd.read_csv(config.TEST_FILE)
     df_test.loc[:, 'answer_start'] = 0
     df_test.loc[:, 'answer_text'] = ''
-    df_test.loc['kfold'] = 0
+    df_test['kfold'] = 0
     df_test['context'] = df_test['context'].apply(lambda x: ' '.join(x.split()))
     df_test['question'] = df_test['question'].apply(lambda x: ' '.join(x.split()))
 
