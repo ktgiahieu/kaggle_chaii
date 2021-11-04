11db644548177a0bac36f4c8b51d72897bedf06e
Hieu Khuong <ktgiahieu@gmail.com>
2021-11-04 22:03:59 +0700

    add

---
+++ src/1st_level/muril_c1_toext_diff_medi/infer_logit.py
@@ -0,0 +1,126 @@
+import sys
+import pickle
+import os
+import gc
+
+import numpy as np
+import pandas as pd
+import torch
+import transformers
+import tqdm
+
+import config
+import models
+import dataset
+import utils
+
+
+def run():
+    df_test = pd.read_csv(config.TEST_FILE)
+    df_test.loc[:, 'answer_start'] = 0
+    df_test.loc[:, 'answer_text'] = ''
+    df_test['context'] = df_test['context'].apply(lambda x: ' '.join(x.split()))
+    df_test['question'] = df_test['question'].apply(lambda x: ' '.join(x.split()))
+
+    device = torch.device('cuda')
+    model_config = transformers.AutoConfig.from_pretrained(
+        config.MODEL_CONFIG)
+    model_config.output_hidden_states = True
+
+    test_dataset = dataset.ChaiiDataset(
+        fold=0,
+        ids=df_test.id.values,
+        contexts=df_test.context.values,
+        questions=df_test.question.values,
+        answers=df_test.answer_text.values,
+        answer_starts=df_test.answer_start.values,
+        mode='infer')
+
+    data_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        shuffle=False,
+        batch_size=config.VALID_BATCH_SIZE,
+        num_workers=1)
+    
+    predicted_labels_start = []
+    predicted_labels_end = []
+
+        
+    for i in range(config.N_FOLDS):  
+        seed = config.SEEDS[i]
+        model = models.ChaiiModel(conf=model_config, fold=i)
+        model.to(device)
+        model.eval()
+        if config.is_kaggle:
+            if i<=2:
+                model_path = f'{config.TRAINED_MODEL_PATH}-p1/model_{i}_{seed}.bin'
+            else:
+                model_path = f'{config.TRAINED_MODEL_PATH}-p2/model_{i}_{seed}.bin'
+        else:
+            model_path = f'{config.TRAINED_MODEL_PATH}/model_{i}_{seed}.bin'
+        model.load_state_dict(torch.load(model_path, map_location="cuda"))
+
+        predicted_labels_per_fold_start = []
+        predicted_labels_per_fold_end = []
+        with torch.no_grad():
+            tk0 = tqdm.tqdm(data_loader, total=len(data_loader))
+            for bi, d in enumerate(tk0):
+                ids = d['ids']
+                mask = d['mask']
+                start_labels = d['start_labels']
+                end_labels = d['end_labels']
+
+                ids = ids.to(device, dtype=torch.long)
+                mask = mask.to(device, dtype=torch.long)
+                start_labels = start_labels.to(device, dtype=torch.float)
+                end_labels = start_labels.to(device, dtype=torch.float)
+
+                outputs_start, outputs_end = model(ids=ids, mask=mask)
+
+                outputs_start = outputs_start.cpu().detach()
+                outputs_end = outputs_end.cpu().detach()
+
+                predicted_labels_per_fold_start.append(outputs_start)
+                predicted_labels_per_fold_end.append(outputs_end)
+        
+        predicted_labels_per_fold_start = torch.cat(
+            tuple(x for x in predicted_labels_per_fold_start), dim=0)
+        predicted_labels_per_fold_end = torch.cat(
+            tuple(x for x in predicted_labels_per_fold_end), dim=0)
+
+        predicted_labels_start.append(predicted_labels_per_fold_start)
+        predicted_labels_end.append(predicted_labels_per_fold_end)
+
+        del model
+        torch.cuda.empty_cache()
+        gc.collect()
+    
+    # Raw predictions
+    predicted_labels_start = torch.stack(
+        tuple(x for x in predicted_labels_start), dim=0)
+    predicted_labels_start, _  = torch.median(predicted_labels_start, dim=0)
+
+    predicted_labels_end = torch.stack(
+        tuple(x for x in predicted_labels_end), dim=0)
+    predicted_labels_end, _  = torch.median(predicted_labels_end, dim=0)
+
+    # Heatmap 
+    heatmap_logit = utils.postprocess_heatmap_logit(df_test, test_dataset.features, 
+                                                   (predicted_labels_start, predicted_labels_end))
+
+    if not os.path.isdir(f'{config.INFERED_PICKLE_PATH}'):
+        os.makedirs(f'{config.INFERED_PICKLE_PATH}')
+        
+    pickle_name = sys.argv[1]
+    with open(f'{config.INFERED_PICKLE_PATH}/{pickle_name}.pkl', 'wb') as handle:
+        pickle.dump(heatmap_logit, handle)
+
+    del test_dataset
+    del data_loader
+    torch.cuda.empty_cache()
+    gc.collect()
+
+
+if __name__ == '__main__':
+    assert len(sys.argv) > 1, "Please specify output pickle name."
+    run()
---
+++ src/1st_level/rembert_c1_hns_external_medi/config.py
@@ -0,0 +1,79 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning) 
+
+import tokenizers
+from transformers import AutoTokenizer, AutoConfig
+import os
+is_kaggle = 'KAGGLE_URL_BASE' in os.environ
+
+# Paths
+comp_name = 'chaii-hindi-and-tamil-question-answering'
+my_impl = 'chaii-impl'
+my_model_dataset = 'chaii-rembert-c1-hns-external'
+if is_kaggle:
+    TRAINING_FILE = f'../input/{my_impl}/data/train_folds_cleaned_dropped.csv'
+    TEST_FILE = f'../input/{comp_name}/test.csv'
+    SUB_FILE = f'../input/{comp_name}/sample_submission.csv'
+    MODEL_SAVE_PATH = f'.'
+    TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
+    INFERED_PICKLE_PATH = '.'
+
+    MODEL_CONFIG = '../input/google-rembert'
+else: #colab
+    repo_name = 'kaggle_chaii'
+    drive_name = 'Chaii'
+    model_save = 'rembert_c1_hns_external'
+    
+    TRAINING_FILE = f'/content/{repo_name}/data/train_folds_cleaned_dropped.csv'
+    TRAINING_FILE_PICKLE = f'/content/gdrive/MyDrive/Dataset/{drive_name}/oof_prob/rembert_combo1_cls.pkl'
+    TEST_FILE = f'/content/{repo_name}/data/test.csv'
+    SUB_FILE = f'/content/{repo_name}/data/sample_submission.csv'
+    PRETRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/rembert_combo1_external/model.bin'
+    MODEL_SAVE_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
+    TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
+    INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
+
+    MODEL_CONFIG = 'google/rembert'
+
+# Model params
+SEEDS = [1000]
+N_FOLDS = 5
+EPOCHS = 3
+NEGATIVE_POSITIVE_RATIO = 3.0
+
+PATIENCE = None
+EARLY_STOPPING_DELTA = None
+N_REINIT_LAST_LAYERS = 0
+TRAIN_BATCH_SIZE = 2
+VALID_BATCH_SIZE = 2
+ACCUMULATION_STEPS = 8
+MAX_LEN = 384
+DOC_STRIDE = 128
+
+TOKENIZER = AutoTokenizer.from_pretrained(
+    MODEL_CONFIG)
+
+CONF = AutoConfig.from_pretrained(
+    MODEL_CONFIG)
+
+N_LAST_HIDDEN = 4
+BERT_DROPOUT = 0.1
+HIGH_DROPOUT = 0.5
+SOFT_ALPHA = 0.6
+WARMUP_RATIO = 0.1
+
+USE_SWA = False
+SWA_RATIO = 0.9
+SWA_FREQ = 30
+
+SAVE_CHECKPOINT_TYPE = 'best_iter' #'best_iter', 'best_epoch' or 'last_epoch'
+EVAL_SCHEDULE = [
+                (10., 100*ACCUMULATION_STEPS),
+                ]
+
+
+#Layer wise learning rate
+HEAD_LEARNING_RATE = 1e-4
+LEARNING_RATE_LAYERWISE_TYPE = 'exponential' #'linear' or 'exponential'
+LEARNING_RATES_RANGE = [5e-6, 5e-5]
+WEIGHT_DECAY = 0.01
