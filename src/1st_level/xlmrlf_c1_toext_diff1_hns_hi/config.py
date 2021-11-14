e7f685056691f5e127ab5266866f9db01acea0ec
Hieu Khuong <ktgiahieu@gmail.com>
2021-11-14 13:54:44 +0700

    add

--- src/1st_level/xlmrlf_c1_toext_diff1_hns_hi/config.py
+++ src/1st_level/xlmrlf_c1_toext_diff1_hns_hi/config.py
@@ -9,7 +9,7 @@ is_kaggle = 'KAGGLE_URL_BASE' in os.environ
 # Paths
 comp_name = 'chaii-hindi-and-tamil-question-answering'
 my_impl = 'chaii-impl'
-my_model_dataset = 'chaii-muril-apex-ta'
+my_model_dataset = 'chaii-xlmrlf-c1-toext-diff1-hns-hi'
 if is_kaggle:
     TRAINING_FILE = f'../input/{my_impl}/data/train_folds_external_cleaned_dropped.csv'
     TEST_FILE = f'../input/{comp_name}/test.csv'
@@ -18,36 +18,34 @@ if is_kaggle:
     TRAINED_MODEL_PATH = f'../input/{my_model_dataset}'
     INFERED_PICKLE_PATH = '.'
 
-    MODEL_CONFIG = '../input/google-muril-base-cased-squad2'
+    MODEL_CONFIG = '../input/markussagen-xlmrlf-base-4096-squad2-384'
 else: #colab
     repo_name = 'kaggle_chaii'
     drive_name = 'Chaii'
-    model_save = 'muril_apex_ta'
+    model_save = 'xlmrlf_c1_toext_diff1_hns_hi'
     
     TRAINING_FILE = f'/content/{repo_name}/data/train_folds_external_cleaned_dropped.csv'
     TEST_FILE = f'/content/{repo_name}/data/test.csv'
     SUB_FILE = f'/content/{repo_name}/data/sample_submission.csv'
+    TRAINING_FILE_PICKLE = f'/content/gdrive/MyDrive/Dataset/{drive_name}/oof_prob/xlmrlf_c1_toext_diff1.pkl'
+    PRETRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/xlmrlf_c1_toext_diff1'
     MODEL_SAVE_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     TRAINED_MODEL_PATH = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/1st_level/{model_save}'
     INFERED_PICKLE_PATH = f'/content/{repo_name}/pickle'
 
-    MODEL_CONFIG = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/google-muril-base-cased-squad2'
+    MODEL_CONFIG = f'/content/gdrive/MyDrive/Dataset/{drive_name}/model_save/pretrained/markussagen-xlmrlf-base-4096-squad2-384'
 
-DEBUG = False
 # Model params
-SEEDS = [13, 26, 43, 1101, 2022]
+SEEDS = [20, 33, 50, 1008, 2029]
 N_FOLDS = 5
-EPOCHS = 2
-#NEGATIVE_POSITIVE_RATIO = 3.0
-
-SHUFFLE_AUGMENT_RATE = 1.0
+EPOCHS = 1
+NEGATIVE_POSITIVE_RATIO = 1.0
 
 PATIENCE = None
 EARLY_STOPPING_DELTA = None
 N_REINIT_LAST_LAYERS = 0
 TRAIN_BATCH_SIZE = 16
 VALID_BATCH_SIZE = 16
-INFER_BATCH_SIZE = 16
 ACCUMULATION_STEPS = 1
 MAX_LEN = 384
 DOC_STRIDE = 128
@@ -58,24 +56,24 @@ TOKENIZER = AutoTokenizer.from_pretrained(
 CONF = AutoConfig.from_pretrained(
     MODEL_CONFIG)
 
-N_LAST_HIDDEN = [ 8, 10,  6,  9, 12]
+N_LAST_HIDDEN = [6,  10, 4,  12,  8]
 BERT_DROPOUT = 0.1
-HIGH_DROPOUT = [0.35, 0.5, 0.45, 0.4, 0.3]
-SOFT_ALPHA = [0.6, 1.0, 0.9, 0.8, 0.7]
+HIGH_DROPOUT = [0.45 , 0.5, 0.4 , 0.35, 0.3 ]
+SOFT_ALPHA = [0.9, 0.8, 0.6 , 1., 0.7]
 WARMUP_RATIO = 0.1
 
 USE_SWA = False
 SWA_RATIO = 0.9
 SWA_FREQ = 30
 
-SAVE_CHECKPOINT_TYPE = 'best_iter' #'best_iter', 'best_epoch' or 'last_epoch'
+SAVE_CHECKPOINT_TYPE = 'last_epoch' #'best_iter', 'best_epoch' or 'last_epoch'
 EVAL_SCHEDULE = [
                 (10., 200*ACCUMULATION_STEPS),
                 ]
 
 
 #Layer wise learning rate
-HEAD_LEARNING_RATE = 3e-5
+HEAD_LEARNING_RATE = 5e-5
 LEARNING_RATE_LAYERWISE_TYPE = 'exponential' #'linear' or 'exponential'
-LEARNING_RATES_RANGE = [3e-5, 3e-5]
+LEARNING_RATES_RANGE = [1e-5, 4e-5]
 WEIGHT_DECAY = 0.001
